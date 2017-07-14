df8a09d7ee094de873908bd1b022e5db9d9232a3
Ivan Minier <ivanminier@me.com>
2017-07-13 19:19:53 -0700

    Done for the now.

--- cents/ItemToCompare.swift
+++ cents/ItemToCompare.swift
@@ -23,13 +23,11 @@ enum WeightTypes {
     case floz // 0.0634013 gallon, 0.03125 quart, 0.0295735 liter, 0.0625 pint, 29.5735 Milliliter
     case milliliter // 0.000264172 gallon, 0.00105699 quart, 0.001 liter, 0.00211338 pint, 0.033814
     
-    case ton // 2000 pounds, 32000 oz, 907185 grams, 0 milligram
     case pound // 0.000453592 ton, 16 oz, 453.592 gram, 453592 milligram
     case ounce // 0 ton, 0.0625 pound, 28.3495 grams, 28349.5 milligram
     case gram // 0 ton, 0.00220462 pound, 0.035274 ounce, 1000 milligram
     case milligram // 0 ton, 0 pound, 0 ounce, 0.001 gram
     
-    // CONVERT Gal
     func convToGallon(itemToConvert: ItemToCompare) -> ItemToCompare {
         let qrtDiv: Float = 4
         let litDiv: Float = 3.78
@@ -37,7 +35,7 @@ enum WeightTypes {
         let fozDiv: Float = 128
         let milDiv: Float = 3785.41
         
-        var convertedItem = itemToConvert
+        var convertedItem: ItemToCompare = itemToConvert
         
         switch itemToConvert.weightType {
         case .quart:
@@ -73,7 +71,7 @@ enum WeightTypes {
         let fozDiv: Float = 64
         let milDiv: Float = 946.353
         
-        var convertedItem = itemToConvert
+        var convertedItem: ItemToCompare = itemToConvert
         
         switch itemToConvert.weightType {
         case .gallon:
@@ -109,7 +107,7 @@ enum WeightTypes {
         let fozDiv: Float = 33.814
         let milDiv: Float = 1000
         
-        var convertedItem = itemToConvert
+        var convertedItem: ItemToCompare = itemToConvert
         
         switch itemToConvert.weightType {
         case .gallon:
@@ -136,6 +134,113 @@ enum WeightTypes {
             return itemToConvert
         }
         
+    }
+    
+    func convToPint(itemToConvert: ItemToCompare) -> ItemToCompare {
+        let galDiv: Float = 0.125
+        let qrtDiv: Float = 1.05669
+        let litDiv: Float = 0.473176
+        let fozDiv: Float = 16
+        let milDiv: Float = 473.176
+        
+        var convertedItem: ItemToCompare = itemToConvert
+        
+        switch itemToConvert.weightType {
+        case .gallon:
+            convertedItem.weight = itemToConvert.weight / galDiv
+            convertedItem.weightType = .pint
+            return convertedItem
+        case .quart:
+            convertedItem.weight = itemToConvert.weight / qrtDiv
+            convertedItem.weightType = .pint
+            return convertedItem
+        case .liter:
+            convertedItem.weight = itemToConvert.weight / litDiv
+            convertedItem.weightType = .pint
+            return convertedItem
+        case .floz:
+            convertedItem.weight = itemToConvert.weight / fozDiv
+            convertedItem.weightType = .pint
+            return convertedItem
+        case .milliliter:
+            convertedItem.weight = itemToConvert.weight / milDiv
+            convertedItem.weightType = .pint
+            return convertedItem
+        default:
+            return itemToConvert
+        }
+        
+    }
+    
+    func convToFloz(itemToConvert: ItemToCompare) -> ItemToCompare {
+        let galDiv: Float = 0.0634013
+        let qrtDiv: Float = 0.03125
+        let litDiv: Float = 0.0295735
+        let pinDiv: Float = 0.0625
+        let milDiv: Float = 29.5735
+        
+        var convertedItem: ItemToCompare = itemToConvert
+        
+        switch itemToConvert.weightType {
+        case .gallon:
+            convertedItem.weight = itemToConvert.weight / galDiv
+            convertedItem.weightType = .floz
+            return convertedItem
+        case .quart:
+            convertedItem.weight = itemToConvert.weight / qrtDiv
+            convertedItem.weightType = .floz
+            return convertedItem
+        case .liter:
+            convertedItem.weight = itemToConvert.weight / litDiv
+            convertedItem.weightType = .floz
+            return convertedItem
+        case .pint:
+            convertedItem.weight = itemToConvert.weight / pinDiv
+            convertedItem.weightType = .floz
+            return convertedItem
+        case .milliliter:
+            convertedItem.weight = itemToConvert.weight / milDiv
+            convertedItem.weightType = .floz
+            return convertedItem
+        default:
+            return itemToConvert
+        }
+        
+    }
+    
+    func convToMilliliter(itemToConvert: ItemToCompare) -> ItemToCompare {
+        let galDiv: Float = 0.000264172
+        let qrtDiv: Float = 0.00105699
+        let litDiv: Float = 0.001
+        let pinDiv: Float = 0.00211338
+        let fozDiv: Float = 0.033814
+        
+        var convertedItem: ItemToCompare = itemToConvert
+        
+        switch itemToConvert.weightType {
+        case .gallon:
+            convertedItem.weight = itemToConvert.weight / galDiv
+            convertedItem.weightType = .milliliter
+            return convertedItem
+        case .quart:
+            convertedItem.weight = itemToConvert.weight / qrtDiv
+            convertedItem.weightType = .milliliter
+            return convertedItem
+        case .liter:
+            convertedItem.weight = itemToConvert.weight / litDiv
+            convertedItem.weightType = .milliliter
+            return convertedItem
+        case .pint:
+            convertedItem.weight = itemToConvert.weight / pinDiv
+            convertedItem.weightType = .milliliter
+            return convertedItem
+        case .floz:
+            convertedItem.weight = itemToConvert.weight / fozDiv
+            convertedItem.weightType = .milliliter
+            return convertedItem
+        default:
+            return itemToConvert
+        }
     }
     
 }
