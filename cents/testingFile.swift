e3fe216706ac8996c1fa8a95e5c4419437bc4358
Ivan Minier <ivanminier@me.com>
2017-07-14 13:09:41 -0700

    test

---
+++ cents/testingFile.swift
@@ -0,0 +1,11 @@
+//
+//  testingFile.swift
+//  cents
+//
+//  Created by Ivan Minier on 7/14/17.
+//  Copyright © 2017 Ivan Minier. All rights reserved.
+//
+
+import Foundation
+
+// testing the commit of this new file to the project online after the break.
